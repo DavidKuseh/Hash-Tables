d08f7b06ba9f7c62682d457f21546ec7efa49669
David Kuseh <davegru203@gmail.com>
2020-03-10 17:55:31 +0000

    complete hash_djb2 function

--- src/hashtable.py
+++ src/hashtable.py
@@ -32,7 +32,14 @@ class HashTable:
 
         OPTIONAL STRETCH: Research and implement DJB2
         '''
-        pass
+        # set the ahs value to 5381
+        hash_value = 5381
+        # iterate over each char in the key
+        for char in key:
+        # set the hash value to the bit shift left by 5 of the hash value and sum of the hash value then add the value for the char 
+            hash_value = ((hash_value << 5) + hash_value) + char
+        # return the hash value
+        return hash_value
 
 
     def _hash_mod(self, key):
